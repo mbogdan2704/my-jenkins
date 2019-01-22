b6336d8aa2b871a1754ee1cc53a8379c9ff199c4
mbogdan2704 <costin_bogdan.maria@stud.acs.upb.ro>
2019-01-22 12:42:07 +0200

    Update application.py

--- application.py
+++ application.py
@@ -110,7 +110,7 @@ lalal
 </head>
 <body id="sample">
   <div class="textColumn">
-    <h1>Congratulations</h1>
+    <h1>BOGDAN BOGDAN BOGDAN</h1>
     <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
   </div>
 
